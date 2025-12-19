782227e61ee49fca9422f0550ac62c598b502bf7
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2025-12-19 01:22:26 +0000

    Primeiro Assignment

---
+++ Minha-pasta/01/assignments.js
@@ -0,0 +1,7 @@
+let country = "Chile";
+let continent = "America do Sul"
+let populacao = "18 milhões"
+
+console.log(country)
+console.log(continent)
+console.log(populacao)
---
+++ Minha-pasta/01/script_eu.js
@@ -0,0 +1,5 @@
+let js = "Legal"
+if(js === "Legal") alert("JavaScript funcionando!")
+console.log("JavaScript funcionando!")
+console.log(10+13-5) 
+
