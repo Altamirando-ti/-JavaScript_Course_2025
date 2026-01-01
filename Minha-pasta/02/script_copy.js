5aecac9d94b5adb2204ce59a3acde86f62f9b8ea
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2026-01-01 17:15:36 +0000

    Iniciando Fundamentals Part 2

---
+++ Minha-pasta/02/script_copy.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let temLincencaMotorista = false;
+const passouTeste = true;
+
+// if(passouTeste) temLincençaMotorista = true;// errado de propósito para o strict mode identificar
+if(passouTeste) temLincencaMotorista = true;
+if(temLincencaMotorista) console.log('Eu posso dirigir');
+
+//Palavras restritas/reservadas ao sistema somente no strict mode
+// const interface = 'Audio';
+// const private = 123;
+
