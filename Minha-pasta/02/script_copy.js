dfee94c1c07a60ac5c479b5263c306c09d2b550f
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2026-01-03 03:10:42 +0000

    Commit esquecido

--- Minha-pasta/02/script_copy.js
+++ Minha-pasta/02/script_copy.js
@@ -1,6 +1,7 @@
+'use strict';
 //----------- 33 - Activating Strict Mode ---------------
 console.log('----------- 33 - Activating Strict Mode ---------------')
-'use strict';
+
 
 let temLincencaMotorista = false;
 const passouTeste = true;
@@ -14,12 +15,13 @@ if(temLincencaMotorista) console.log('Eu posso dirigir');
 // const private = 123;
 
 //----------- 34 - Functions ---------------
+console.log('----------- 34 - Functions ---------------')
 function semEntrada(){
     console.log('Função sem entrada executada')
 }
 semEntrada(); //calling/involking/running
 
-console.log('----------- 34 - Functions ---------------')
+
 function suco(nMaca, nLaranja){
     console.log(nMaca,nLaranja)
     let sucoPronto = `Seu suco possui ${nMaca} maçãs e ${nLaranja} laranjas`
@@ -29,3 +31,25 @@ function suco(nMaca, nLaranja){
 console.log(suco(5, 6))
 // let pedidoSuco = suco(5, 6);
 // console.log(pedidoSuco)
+
+//----------- 35 - Function Declarations vs. Expressions ---------------
+console.log('----------- 35 - Function Declarations vs. Expressions ---------------')
+
+//Function declaration
+const dataNascimento = Number(prompt("Qual a sua data de nascimento?"))
+console.log(`A idade do fulano é ${calcIdade(dataNascimento)} por Function declaration`)
+
+function calcIdade(dataNasc){
+    // const idade = 2026 - dataNasc;
+    return 2026 - dataNasc;
+}
+
+
+//Function Expression ou Anonimous Function
+const dataNascimento2 = Number(prompt("Qual a sua data de nascimento?"))
+console.log(`A idade do fulano é ${calcIdade2(dataNascimento2)} por Anonimous function`)
+
+const calcIdade2 = function(dataNasc){
+    return 2026 - dataNasc;
+}
+
