cd8afc2ba80b4bfe08257e84733dfa2c3060877e
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2026-01-07 01:11:45 +0000

    Challenge 2 part 2 feito

--- Minha-pasta/02/challenge_part2.js
+++ Minha-pasta/02/challenge_part2.js
@@ -23,3 +23,27 @@ avgDolphins = calcAverage(85,54,41);
 avgKoalas = calcAverage(23,34,27);
 console.log(avgDolphins, avgKoalas);
 console.log(checkWinner(avgDolphins,avgKoalas));
+
+//----------- Challenge 2 - Part 2 ---------------
+console.log('----------- Challenge 2 - Part 2 ---------------')
+// let bill = Number(prompt("Digite o valor da conta:"))
+let bill = 100
+console.log(bill)
+
+let calcTip = function(bill){
+    let tip = bill >=50 && bill <=300 ? bill * 15/100 : bill * 20/100;
+    return tip
+}
+
+let calculationTip = calcTip(bill)
+console.log(`O valor da conta é de ${bill} e o valor da gorjeta é de ${calculationTip} sendo o total a pagar de ${calculationTip + bill}`)
+
+const bills = [125, 555, 44]
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+console.log(tips)
+console.log(`O valor da conta é de ${bills[0]} e o valor da gorjeta é de ${calcTip(bills[0])} sendo o total a pagar de ${calcTip(bills[0]) + bills[0]}`)
+console.log(`O valor da conta é de ${bills[1]} e o valor da gorjeta é de ${calcTip(bills[1])} sendo o total a pagar de ${calcTip(bills[1]) + bills[1]}`)
+console.log(`O valor da conta é de ${bills[2]} e o valor da gorjeta é de ${calcTip(bills[2])} sendo o total a pagar de ${calcTip(bills[2]) + bills[2]}`)
+
+let arrayTotal = new Array(calcTip(bills[0]) + bills[0], calcTip(bills[1]) + bills[1],calcTip(bills[2]) + bills[2])
+console.log(arrayTotal)
