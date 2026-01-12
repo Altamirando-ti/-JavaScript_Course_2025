c83bb681492d79ddaffc65cd68af66166bdeaa50
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2026-01-12 21:41:27 +0000

    Pasta 3 criada - Section 5 - Video 57 - Extensão Prettier

---
+++ Minha-pasta/03/script_copy.js
@@ -0,0 +1,12 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+/* Foi instalada as extensão:
+ Prettier
+ */
+// é Necessário salvar para que o prettier faça as alterações automáticas
+const x = "25"; // as àspas foram colocadas automaticamente
+const teste = (nasc) => 2025 - nasc; // O parenteses foi colocado automaticamente
+//Aruivo -> Preferências -> Configurar Snippets
+console.log(); // basta escrever cl
+
