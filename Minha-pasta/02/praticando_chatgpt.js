e4da0a664ed1a47857ac0ca864c8afb486a221dc
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2026-01-14 15:44:04 +0000

    Finalizando praticando com chatGPT

--- Minha-pasta/02/praticando_chatgpt.js
+++ Minha-pasta/02/praticando_chatgpt.js
@@ -52,4 +52,134 @@ for(let i=0; i<tempC.length; i++){
 }
 // fahrenheit(tempC)
 
+/*
+----------4)Somar valores do array--------
+Crie uma função somarArray que:
+Receba um array de números
+Retorne a soma de todos os valores
+ */
 
+
+/*
+ ---------5) Contar elementos pares--------
+Crie uma função contarPares que:
+Receba um array de números
+Retorne quantos números pares existem no array
+ */
+console.log('---------4)Contar elementos pares--------')
+// const matriz = [0,1,2,3,4,5,6];
+const matriz = []
+const tamanho = 10
+for(let j=0; j< tamanho; j++){ // loop cria uma matriz com números randômicos
+    matriz.push(Math.trunc(Math.random()*tamanho))
+}
+console.log(matriz)
+let contPar=0;//contador
+let functionParImpar = function(arg){ //função define número do array par ou ímpar
+    if(arg % 2 ===0){
+        // console.log('é par!');
+        contPar = contPar +1;
+        return 'é par!'
+    } else {
+        // console.log('é ìmpar!');
+        return 'é ìmpar!';
+    }
+}
+
+const parImpar =[]
+for(let i=0; i < matriz.length; i++){  // loop cria matriz com
+    parImpar.push(matriz[i],functionParImpar(matriz[i]))
+}
+console.log(parImpar)
+console.log(`Foram computados ${contPar} números pares`)
+
+/*
+---------6) Maior número do array -------
+Crie uma função maiorNumero que:
+Receba um array de números
+Retorne o maior número do array */
+console.log('---------6) Maior número do array -------')
+
+/*
+---------- 7) Criar um objeto pessoa ---------
+Crie um objeto chamado pessoa com as propriedades:
+nome
+idade
+cidade
+Depois:
+Mostre no console uma frase como:
+"Meu nome é João, tenho 25 anos e moro em São Paulo" */
+console.log('---------- 7) Criar um objeto pessoa ---------')
+const objPessoa ={
+ nome: 'Paula Coelho',
+ idade: 25,
+ cidade: 'Uberlândia'
+}
+// console.log(objPessoa)
+console.log(`Meu nome é ${objPessoa.nome}, tenho ${objPessoa.idade} anos e moro em ${objPessoa.cidade}`)
+
+/*
+Atualizar propriedade do objeto
+Usando o objeto pessoa:
+Altere a idade da pessoa
+Mostre a nova idade no console */
+objPessoa.idade = 30;
+console.log(`Meu nome é ${objPessoa.nome}, tenho ${objPessoa.idade} anos e moro em ${objPessoa.cidade}`)
+
+/*Objeto calculadora
+Crie um objeto calculadora com os métodos:
+somar(a, b)
+subtrair(a, b)
+multiplicar(a, b) */
+const objCalculadora = {
+    a: 1,
+    b: 2,
+
+    somar: function(){
+        this.somaAB = this.a + this.b;
+        return this.somaAB;
+    },
+
+    subtrair: function(){
+        this.subtracaoAB = this.a - this.b;
+        return this.subtracaoAB;
+    },
+
+    multiplicar: function(){
+        this.mutiplicacaoAB = this.a*this.b;
+        return this.mutiplicacaoAB;
+    }
+}
+console.log(objCalculadora.somar())
+console.log(objCalculadora.subtrair())
+console.log(objCalculadora.multiplicar())
+console.log(objCalculadora)
+//Explicação: Este objeto possui os seus métodos que armazenam o resultados destes
+// nas novas propriedades somaAB, subtracaoAB e mutiplicacaoAB criadas pelos pŕoprios métodos
+
+// versão chatgpt
+let calculadora = {
+  somar: function(a, b) {
+    return a + b;
+  },
+
+  subtrair: function(a, b) {
+    return a - b;
+  },
+
+  multiplicar: function(a, b) {
+    return a * b;
+  }
+};
+console.log(calculadora.somar(2, 3))
+console.log(calculadora)
+// Explicação: Este objeto possui os seus métodos, mas não armazena os resultados
+// nem novas propriedades
+
+/* ----- 10) Crie um objeto aluno ---------
+nome
+notas (um array de números)
+Um método calcularMedia() que:
+Calcule e retorne a média das notas
+*/ 
+console.log('----- 10) Crie um objeto aluno ---------')
--- Minha-pasta/03/script_copy.js
+++ Minha-pasta/03/script_copy.js
@@ -5,8 +5,26 @@
  Prettier
  */
 // é Necessário salvar para que o prettier faça as alterações automáticas
-const x = "25"; // as àspas foram colocadas automaticamente
-const teste = (nasc) => 2025 - nasc; // O parenteses foi colocado automaticamente
-//Aruivo -> Preferências -> Configurar Snippets
-console.log(); // basta escrever cl
 
+// const x = "25"; // as àspas foram colocadas automaticamente
+// // const teste = nasc => 2025 - nasc;
+// const teste = (nasc) => 2025 - nasc; // O parenteses foi colocado automaticamente
+// //Aruivo -> Preferências -> Configurar Snippets
+// console.log(); // basta escrever cl
+// console.log(teste(1988));
+
+// -------63. Debugging with the Console and Breakpoint------
+console.log("-------63. Debugging with the Console and Breakpoint------");
+const medidaKelvin = function () {
+  const medida = {
+    tipo: "temp",
+    unit: "celcius",
+    value: Number(prompt("Escreva a temperatura em graus Celcius: "))
+  };
+  // console.log(medida.value);// ok!
+//   console.log(medida);// Erro está aqui. O valor no objeto é uma string
+//   console.table(medida); // outra maneira
+  const kelvin = medida.value + 273;
+  return kelvin;
+};
+console.log(medidaKelvin());
