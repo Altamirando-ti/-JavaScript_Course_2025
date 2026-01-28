482328d14cb852169ae23bbf61370b23cc688191
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2026-01-28 03:30:21 +0000

    84. Working with classes

--- Minha-pasta/06 - Modal Section 7/script_copy.js
+++ Minha-pasta/06 - Modal Section 7/script_copy.js
@@ -1,1 +1,38 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+
+const overlay = document.querySelector(".overlay");
+
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const btnCloseModal = document.querySelector(".close-modal");
+
+console.log(btnsOpenModal);
+
+let openModal = function(){
+    modal.classList.remove('hidden');
+    // modal.style.display = 'block' // outra maneira
+    overlay.classList.remove('hidden')
+}
+
+for(let i=0 ; i<btnsOpenModal.length ; i++){
+    // console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// let closeModal = function(){
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden')
+// } 
+
+//Também pode ser por meio de um função declaração
+
+function closeModal(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
---
+++ Minha-pasta/06 - Modal Section 7/teste.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal-overlay')
+const btnOpenModal = document.querySelector('#openModalBtn')
+const btnCloseModal = document.querySelector('.close-btn')
+
+// console.log(modal);
+
+btnOpenModal.addEventListener('click', function(){
+    modalOverlay.classList.add('active')
+    // modalOverlay.style.display = 'flex'
+});
+
+btnCloseModal.addEventListener('click', function(){
+    modalOverlay.classList.remove('active');
+    // modalOverlay.style.display = 'none'
+})  
