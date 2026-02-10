081863958bb9da39c2581c9639d4daa88a1fad9a
ALTAMIRANDO COLOMBO RIBEIRO NETO <altamirando.ti@gmail.com>
2026-02-10 02:19:56 +0000

    Pig game corrigido

--- Minha-pasta/07 - Pig game/script.js
+++ Minha-pasta/07 - Pig game/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const pontos = 20;
+
 const btnRollDice = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const btnNewGame = document.querySelector(".btn--new");
@@ -14,41 +16,46 @@ const frase = document.querySelector(".frase");
 dice.classList.add("hidden");
 score0.textContent = "0";
 score1.textContent = "0";
-pontosParaVencer.textContent = "30";
+pontosParaVencer.textContent = String(pontos);
 
 //Função clicar no Rolldice - funcionando
 let soma = 0;
 let numPlayer = 0;
+let scoreMenorq100 = true;
+
+
 btnRollDice.addEventListener("click", function () {
-  frase.classList.add("hidden");
-  let numDado = rollDice();
-  if (numDado !== 1) {
-    soma = soma + numDado;
-    // currentScore0.textContent = soma;
-    // currenteScore aciona dinamicamente
-    document.getElementById(`current--${numPlayer}`).textContent = soma;
-  } else {
-    //Switch to next player
-    if (numPlayer === 0) {
-      soma = 0;
+  if (scoreMenorq100) {
+    frase.classList.add("hidden");
+    let numDado = rollDice();
+    if (numDado !== 1) {
+      soma = soma + numDado;
+      // currentScore0.textContent = soma;
+      // currenteScore aciona dinamicamente
       document.getElementById(`current--${numPlayer}`).textContent = soma;
-      document
-        .querySelector(`.player--${numPlayer}`)
-        .classList.remove("player--active");
-      numPlayer = 1;
-      document
-        .querySelector(`.player--${numPlayer}`)
-        .classList.add("player--active");
     } else {
-      soma = 0;
-      document.getElementById(`current--${numPlayer}`).textContent = soma;
-      document
-        .querySelector(`.player--${numPlayer}`)
-        .classList.remove("player--active");
-      numPlayer = 0;
-      document
-        .querySelector(`.player--${numPlayer}`)
-        .classList.add("player--active");
+      //Switch to next player
+      if (numPlayer === 0) {
+        soma = 0;
+        document.getElementById(`current--${numPlayer}`).textContent = soma;
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.remove("player--active");
+        numPlayer = 1;
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.add("player--active");
+      } else {
+        soma = 0;
+        document.getElementById(`current--${numPlayer}`).textContent = soma;
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.remove("player--active");
+        numPlayer = 0;
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.add("player--active");
+      }
     }
   }
 });
@@ -65,58 +72,62 @@ function rollDice() {
 // Botão Hold
 let soma0 = 0;
 let soma1 = 0;
-const pontos = 30;
+
 btnHold.addEventListener("click", function () {
-  if (numPlayer === 0) {
-    soma0 = soma0 + soma;
-    score0.textContent = soma0;
+  if (scoreMenorq100) {
+    if (numPlayer === 0) {
+      soma0 = soma0 + soma;
+      score0.textContent = soma0;
 
-    // Verificar se o jogador 1 ganhou
-    if (soma0 >= pontos) {
-      // console.log("Jogador 1 ganhou!");
-      // window.alert("Jogador 1 ganhou!");
+      // Verificar se o jogador 1 ganhou
+      if (soma0 >= pontos) {
+        scoreMenorq100 = false;
+        // console.log("Jogador 1 ganhou!");
+        // window.alert("Jogador 1 ganhou!");
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.remove("player--active");
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.add("player--winner");
+        return; // Para a execução
+      }
+      soma = 0;
+      document.getElementById(`current--${numPlayer}`).textContent = soma;
       document
         .querySelector(`.player--${numPlayer}`)
         .classList.remove("player--active");
+      numPlayer = 1;
       document
         .querySelector(`.player--${numPlayer}`)
-        .classList.add("player--winner");
-      return; // Para a execução
-    }
-    soma = 0;
-    document.getElementById(`current--${numPlayer}`).textContent = soma;
-    document
-      .querySelector(`.player--${numPlayer}`)
-      .classList.remove("player--active");
-    numPlayer = 1;
-    document
-      .querySelector(`.player--${numPlayer}`)
-      .classList.add("player--active");
-  } else {
-    soma1 = soma1 + soma;
-    score1.textContent = soma1;
+        .classList.add("player--active");
+    } else {
+      soma1 = soma1 + soma;
+      score1.textContent = soma1;
 
-    // Verificar se o jogador 2 ganhou
-    if (soma1 >= pontos) {
-      // console.log("Jogador 2 ganhou!");
-      // window.alert("Jogador 2 ganhou!");
+      // Verificar se o jogador 2 ganhou
+      if (soma1 >= pontos) {
+        scoreMenorq100 = false;
+        // console.log("Jogador 2 ganhou!");
+        // window.alert("Jogador 2 ganhou!");
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.remove("player--active");
+        document
+          .querySelector(`.player--${numPlayer}`)
+          .classList.add("player--winner");
+        return; // Para a execução  
+      }
+      soma = 0;
+      document.getElementById(`current--${numPlayer}`).textContent = soma;
       document
         .querySelector(`.player--${numPlayer}`)
         .classList.remove("player--active");
+      numPlayer = 0;
       document
         .querySelector(`.player--${numPlayer}`)
-        .classList.add("player--winner");
-      return; // Para a execução
+        .classList.add("player--active");
     }
-    soma = 0;
-    document.getElementById(`current--${numPlayer}`).textContent = soma;
-    document
-      .querySelector(`.player--${numPlayer}`)
-      .classList.remove("player--active");
-    numPlayer = 0;
-    document
-      .querySelector(`.player--${numPlayer}`)
-      .classList.add("player--active");
   }
 });
 
@@ -125,15 +136,14 @@ btnNewGame.addEventListener("click", () => {
   soma0 = 0;
   soma1 = 0;
   soma = 0;
+  scoreMenorq100 = true;
   currentScore0.textContent = 0;
   currentScore1.textContent = 0;
   score0.textContent = 0;
   score1.textContent = 0;
   dice.classList.add("hidden");
   frase.classList.remove("hidden");
-  document
-    .querySelector(`.player--${numPlayer}`)
-    .classList.remove("player--winner");
+  document.querySelector(`.player--${numPlayer}`).classList.remove("player--winner");
   numPlayer = 0;
   document.querySelector(`.player--1`).classList.remove("player--active");
   document.querySelector(`.player--0`).classList.add("player--active");
